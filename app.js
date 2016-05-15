0c1e7388432d7b85009205bb84ad4a5db983b680
Cary Lee <carylee@gmail.com>
2016-05-15 09:29:03 -0700

    Redirect /api to bare, switch based on Content-Type

--- app.js
+++ app.js
@@ -11,22 +11,37 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/api/petitions/:id', function (req, res) {
-  var id = req.param('id');
+app.get('/api/petitions/:id', function(req, res) {
+  res.redirect('/petitions/' + req.params.id);
+});
+
+app.get('/petitions/:id', function (req, res) {
+  var id = req.params.id;
   if (id.length === 5) {
     id = id.substring(2);
   }
 
-  res.header("Content-Type", "text/plain");
-  request('http://calms2016.umc.org/Text.aspx?mode=Petition&Number=' + id, function(error, response, body) {
-    $ = cheerio.load(body);
-    var html = $("#innercontent_text").find("table").remove().end().html()
-    res.send(markdownify(html));
-  });
+  var url = 'http://calms2016.umc.org/Text.aspx?mode=Petition&Number=' + id;
+
+  if (req.is("text/plain")) {
+    res.header("Content-Type", "text/plain");
+    request(url, function(error, response, body) {
+      $ = cheerio.load(body);
+      var html = $("#innercontent_text").find("table").remove().end().html()
+      res.send(markdownify(html));
+    });
+  } else {
+    res.header("Content-Type", "text/html");
+    request(url, function(error, response, body) {
+      $ = cheerio.load(body);
+      var html = $("#innercontent_text").find("table").remove().end().html()
+      res.send(html);
+    });
+  }
 });
 
 app.listen(port, function () {
-    console.log('UMC GC app listening on port 3000!');
+    console.log('UMC GC app listening on port ' + port + '!');
 });
 
 function stripExtraHtml(html) {
