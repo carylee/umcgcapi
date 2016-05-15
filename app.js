4257f01751e6b1907fd7f417f89878907a72a70f
Cary Lee <carylee@gmail.com>
2016-05-15 08:58:26 -0700

    Add CORS headers

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var cheerio = require('cheerio');
 var toMarkdown = require('to-markdown');
 var port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/petitions/:id', function (req, res) {
   var id = req.param('id');
   if (id.length === 5) {
